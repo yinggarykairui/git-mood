b876d0079023cae05d1ee9e7f7fc6e65a3e732bf
Kairui Ying <yinggarykairui@gmail.com>
2026-08-05 19:18:23 +0000

    scaffold: LICENSE, gitignore, entry point and git-mood symlink

---
+++ git_mood.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""git-mood - a terminal mood chart for a git repository."""
+
+VERSION = "1.0"
+
+
+def main(argv):
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
